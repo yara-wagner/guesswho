cb38e3823ec4c3405030f0cef3dda18a581d9bf6
Yara <yara.wagner@ergon.ch>
2026-08-24 10:43:14 +0200

    add animal character set

---
+++ js/character_data.js
@@ -0,0 +1,225 @@
+// =============================
+// CHARACTER DATA
+// =============================
+
+// Reine Datenlisten pro Set. Die Logik dazu steht in characters.js.
+
+const disneyCharacters = [
+  {
+    id: 1,
+    name: "Aladdin",
+    image: "src/disney/aladdin.avif",
+  },
+
+  {
+    id: 2,
+    name: "Ariel",
+    image: "src/disney/ariel.avif",
+  },
+
+  {
+    id: 3,
+    name: "Bambi",
+    image: "src/disney/bambi.avif",
+  },
+
+  {
+    id: 4,
+    name: "Cinderella",
+    image: "src/disney/cinderella.avif",
+  },
+
+  {
+    id: 5,
+    name: "Donald Duck",
+    image: "src/disney/donald_duck.avif",
+  },
+
+  {
+    id: 6,
+    name: "Dumbo",
+    image: "src/disney/dumbo.avif",
+  },
+
+  {
+    id: 7,
+    name: "Goofy",
+    image: "src/disney/goofy.avif",
+  },
+
+  {
+    id: 8,
+    name: "Hercules",
+    image: "src/disney/hercules.avif",
+  },
+
+  {
+    id: 9,
+    name: "Lightning McQueen",
+    image: "src/disney/lightning_mcqueen.avif",
+  },
+
+  {
+    id: 10,
+    name: "Merida",
+    image: "src/disney/merida.avif",
+  },
+
+  {
+    id: 11,
+    name: "Mickey Mouse",
+    image: "src/disney/mickey_mouse.avif",
+  },
+
+  {
+    id: 12,
+    name: "Mike",
+    image: "src/disney/mike.avif",
+  },
+
+  {
+    id: 13,
+    name: "Moana",
+    image: "src/disney/moana.avif",
+  },
+
+  {
+    id: 14,
+    name: "Olaf",
+    image: "src/disney/olaf.avif",
+  },
+
+  {
+    id: 15,
+    name: "Pinocchio",
+    image: "src/disney/pinocchio.avif",
+  },
+
+  {
+    id: 16,
+    name: "Stitch",
+    image: "src/disney/stitch.avif",
+  },
+
+  {
+    id: 17,
+    name: "Tinker Bell",
+    image: "src/disney/tinker_bell.avif",
+  },
+
+  {
+    id: 18,
+    name: "Winnie The Pooh",
+    image: "src/disney/winnie_the_pooh.avif",
+  },
+];
+
+const animalsCharacters = [
+  {
+    id: 1,
+    name: "Giraffe",
+    image: "src/animals/giraffe.jpg",
+  },
+
+  {
+    id: 2,
+    name: "Racoon",
+    image: "src/animals/racoon.jpg",
+  },
+
+  {
+    id: 3,
+    name: "Tiger",
+    image: "src/animals/tiger.jpg",
+  },
+
+  {
+    id: 4,
+    name: "Fox",
+    image: "src/animals/fox.jpg",
+  },
+
+  {
+    id: 5,
+    name: "Rabbit",
+    image: "src/animals/rabbit.jpg",
+  },
+
+  {
+    id: 6,
+    name: "Wolf",
+    image: "src/animals/wolf.jpg",
+  },
+
+  {
+    id: 7,
+    name: "Cow",
+    image: "src/animals/cow.jpg",
+  },
+
+  {
+    id: 8,
+    name: "Cat",
+    image: "src/animals/cat.jpg",
+  },
+
+  {
+    id: 9,
+    name: "Dog",
+    image: "src/animals/dog.jpg",
+  },
+
+  {
+    id: 10,
+    name: "Elephant",
+    image: "src/animals/elephant.jpg",
+  },
+
+  {
+    id: 11,
+    name: "Fish",
+    image: "src/animals/fish.jpg",
+  },
+
+  {
+    id: 12,
+    name: "Crocodile",
+    image: "src/animals/crocodile.jpg",
+  },
+
+  {
+    id: 13,
+    name: "Pig",
+    image: "src/animals/pig.jpg",
+  },
+
+  {
+    id: 14,
+    name: "Snake",
+    image: "src/animals/snake.jpg",
+  },
+
+  {
+    id: 15,
+    name: "Sheep",
+    image: "src/animals/sheep.jpg",
+  },
+
+  {
+    id: 16,
+    name: "Lion",
+    image: "src/animals/lion.jpg",
+  },
+
+  {
+    id: 17,
+    name: "Penguin",
+    image: "src/animals/penguin.jpg",
+  },
+
+  {
+    id: 18,
+    name: "Seal",
+    image: "src/animals/seal.jpg",
+  },
+];
--- js/characters.js
+++ js/characters.js
@@ -1,123 +1,10 @@
 // =============================
-// CHARACTER DATA
+// CHARACTER SETS
 // =============================
 
 // Die Charaktere sind in Sets gruppiert. Pro Spiel wird genau ein Set
 // gespielt, die Auswahl passiert auf character_sets.html.
-
-const disneyCharacters = [
-  {
-    id: 1,
-    name: "Aladdin",
-    image: "src/disney/aladdin.avif",
-  },
-
-  {
-    id: 2,
-    name: "Ariel",
-    image: "src/disney/ariel.avif",
-  },
-
-  {
-    id: 3,
-    name: "Bambi",
-    image: "src/disney/bambi.avif",
-  },
-
-  {
-    id: 4,
-    name: "Cinderella",
-    image: "src/disney/cinderella.avif",
-  },
-
-  {
-    id: 5,
-    name: "Donald Duck",
-    image: "src/disney/donald_duck.avif",
-  },
-
-  {
-    id: 6,
-    name: "Dumbo",
-    image: "src/disney/dumbo.avif",
-  },
-
-  {
-    id: 7,
-    name: "Goofy",
-    image: "src/disney/goofy.avif",
-  },
-
-  {
-    id: 8,
-    name: "Hercules",
-    image: "src/disney/hercules.avif",
-  },
-
-  {
-    id: 9,
-    name: "Lightning McQueen",
-    image: "src/disney/lightning_mcqueen.avif",
-  },
-
-  {
-    id: 10,
-    name: "Merida",
-    image: "src/disney/merida.avif",
-  },
-
-  {
-    id: 11,
-    name: "Mickey Mouse",
-    image: "src/disney/mickey_mouse.avif",
-  },
-
-  {
-    id: 12,
-    name: "Mike",
-    image: "src/disney/mike.avif",
-  },
-
-  {
-    id: 13,
-    name: "Moana",
-    image: "src/disney/moana.avif",
-  },
-
-  {
-    id: 14,
-    name: "Olaf",
-    image: "src/disney/olaf.avif",
-  },
-
-  {
-    id: 15,
-    name: "Pinocchio",
-    image: "src/disney/pinocchio.avif",
-  },
-
-  {
-    id: 16,
-    name: "Stitch",
-    image: "src/disney/stitch.avif",
-  },
-
-  {
-    id: 17,
-    name: "Tinker Bell",
-    image: "src/disney/tinker_bell.avif",
-  },
-
-  {
-    id: 18,
-    name: "Winnie The Pooh",
-    image: "src/disney/winnie_the_pooh.avif",
-  },
-];
-
-// =============================
-// CHARACTER SETS
-// =============================
+// Die Charakter-Listen selber stehen in character_data.js.
 
 // available: false -> die Kachel wird ausgegraut und ist nicht anklickbar
 
@@ -133,9 +20,9 @@ const characterSets = [
   {
     id: "coming-soon-1",
     name: "Animals",
-    icon: "🔒",
-    available: false,
-    characters: [],
+    icon: "🐾",
+    available: true,
+    characters: animalsCharacters,
   },
 
   {
